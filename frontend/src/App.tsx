8f1b1d31d5cfd4f32051863e8972548ae1e531da
Sabine <sabinehernandes@gmail.com>
2024-11-17 21:57:51 +0100

    import MainNavigation component into App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import MainNavigation from "./components/landing-page/MainNavigation";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <MainNavigation />
     </>
   );
 }
