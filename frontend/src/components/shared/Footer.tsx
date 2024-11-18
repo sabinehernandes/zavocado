6e18dabfde7ceb6c86c3076b22e38e2a74e7788a
Sabine <sabinehernandes@gmail.com>
2024-11-18 14:03:49 +0100

    change footer text and style

--- frontend/src/components/shared/Footer.tsx
+++ frontend/src/components/shared/Footer.tsx
@@ -2,11 +2,10 @@ export default function Footer() {
   return (
     <footer className="footer footer-center bg-slate-100 font-light text-stone-600 p-4">
       <aside>
-        <p>
-          Copyright © {new Date().getFullYear()} - All right reserved by
-          Zavocado
+        <p className="font-bold">
+          © {new Date().getFullYear()} Copyright Zava Farms Zavocado
         </p>
-        <p>Created by Sabine Hernandes</p>
+        <p className="-m-1">Created by Sabine Hernandes</p>
       </aside>
     </footer>
   );
