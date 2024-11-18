30bed8845d3fc253c10b0c627612ac92ac025793
Sabine <sabinehernandes@gmail.com>
2024-11-18 15:36:21 +0100

    create AdminPage component and import into admin.tsx

--- frontend/src/routes/admin.tsx
+++ frontend/src/routes/admin.tsx
@@ -1,10 +1,11 @@
-import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
+import * as React from "react";
+import { createFileRoute } from "@tanstack/react-router";
+import AdminPage from "../components/admin/AdminPage";
 
-export const Route = createFileRoute('/admin')({
+export const Route = createFileRoute("/admin")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return 'Hello /admin!'
+  return <AdminPage />;
 }
