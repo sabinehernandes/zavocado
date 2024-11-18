a195044b091dfd6ec54cd5f086b7f205c04835c1
Sabine <sabinehernandes@gmail.com>
2024-11-18 09:15:21 +0100

    move Navbar to shared folder and update imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import MainNavigation from "./components/landing-page/MainNavigation";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/shared/Navbar";
+import Footer from "./components/shared/Footer";
 
 export default function App() {
   return (
     <>
       <Navbar />
       <MainNavigation />
+      <Footer />
     </>
   );
 }
