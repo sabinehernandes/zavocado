28dfe39d5de1a08206d68cb5e17c9a3ab0204a84
Sabine <sabinehernandes@gmail.com>
2024-11-18 13:53:21 +0100

    add main section and import ProductCard component

--- frontend/src/components/products-page/ProductsPage.tsx
+++ frontend/src/components/products-page/ProductsPage.tsx
@@ -1,7 +1,19 @@
+import ProductCard from "./ProductCard";
+
 export default function ProductsPage() {
   return (
     <>
-      <h1>hi</h1>
+      <main className="bg-slate-100 text-center h-96 flex flex-col items-center justify-center space-y-4">
+        <h1 className="text-5xl font-bold text-stone-600 tracking-wide bg-slate-100">
+          Enjoy our finest avocado selection
+        </h1>
+      </main>
+      <section className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-3 lg:grid-rows-3 gap-3 m-10">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </section>
     </>
   );
 }
