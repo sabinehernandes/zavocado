d71e3d72356437972a172b8c90866655c1e1542d
Sabine <sabinehernandes@gmail.com>
2024-11-19 14:12:08 +0100

    create AvocadoService with crud operations

---
+++ backend/src/main/java/zavocado/backend/service/AvocadoService.java
@@ -0,0 +1,37 @@
+package zavocado.backend.service;
+
+import org.springframework.stereotype.Service;
+import zavocado.backend.model.Avocado;
+import zavocado.backend.model.AvocadoDbRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AvocadoService {
+
+    private final AvocadoDbRepository repository;
+
+    public AvocadoService(AvocadoDbRepository repository) {
+        this.repository = repository;
+    }
+
+    public Optional<Avocado> findById(String id) {
+        return repository.findById(id);
+    }
+
+    public List<Avocado> findAll() {
+        return repository.findAll();
+    }
+
+    public Avocado save(Avocado avocado) {
+        return repository.save(avocado);
+    }
+
+    public void deleteById(String id) throws Exception {
+        if (!repository.existsById(id)) {
+            throw new Exception("Avocado not found. Id: " + id);
+        }
+        repository.deleteById(id);
+    }
+}
