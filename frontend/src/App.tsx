b1fe688af4c54e0998a908481f89e9e88bb10969
Sabine <sabinehernandes@gmail.com>
2024-11-20 09:30:24 +0100

    setup queryClient on App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 import MainNavigation from "./components/landing-page/MainNavigation";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <MainNavigation />
-    </>
+    </QueryClientProvider>
   );
 }
