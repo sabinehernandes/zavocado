1a661eb99ad5830e0f2a34e56db2e7e97c6dc637
Sabine <sabinehernandes@gmail.com>
2024-11-20 10:15:46 +0100

    rendering avocados on ProductCard

--- frontend/src/components/products-page/ProductCard.tsx
+++ frontend/src/components/products-page/ProductCard.tsx
@@ -7,34 +7,23 @@ interface ProductCardProps {
   description: string;
 }
 
-const ProductCard: React.FC<ProductCardProps> = (
-  {
-    // imageUrl,
-    // name,
-    // price,
-    // description,
-  }
-) => {
+const ProductCard: React.FC<ProductCardProps> = ({
+  imageUrl,
+  name,
+  price,
+  description,
+}) => {
   return (
     <div className="flex border border-gray-300 bg-white p-4 rounded-lg w-96 h-80 items-center justify-center">
       <div className="w-32 h-32 flex-shrink-0">
-        <img
-          src="public/products-page/bacon.jpg"
-          className="w-full h-full object-cover"
-        />
+        <img src={imageUrl} className="w-full h-full object-cover" />
       </div>
       <div className="flex-1 pl-4 flex flex-col justify-between bg-white">
         <div>
-          <h1 className="text-xl font-bold bg-white">Bacon</h1>
-          <p className="text-lg text-gray-700 bg-white">$50</p>
-          <p className="text-gray-600 bg-white">
-            A smoother-skinned variety with thin, light green skin and a milder,
-            less buttery flavor.
-          </p>
+          <h1 className="text-xl font-bold bg-white">{name}</h1>
+          <p className="text-lg text-gray-700 bg-white">${price}</p>
+          <p className="text-gray-600 bg-white">{description}</p>
         </div>
-        {/* <h1 className="text-xl font-bold">{name}</h1>
-        <p className="text-lg text-gray-700">Price: ${price.toFixed(2)}</p>
-        <p className="text-gray-600">{description}</p> */}
         <div className="mt-2 bg-white">
           <label htmlFor="quantity" className="mr-2 bg-white">
             Quantity (kg)
