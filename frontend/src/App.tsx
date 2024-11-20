7bbbf7d142f7cca31523d126002b8111e7cea6ca
Sabine <sabinehernandes@gmail.com>
2024-11-20 09:55:03 +0100

    move QueryClientProvider setup from App to main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,5 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from "@tanstack/react-query";
 import MainNavigation from "./components/landing-page/MainNavigation";
 
-const queryClient = new QueryClient();
-
 export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <MainNavigation />
-    </QueryClientProvider>
-  );
+  return <MainNavigation />;
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,13 @@
 import { createRoot } from "react-dom/client";
 import { routeTree } from "./routeTree.gen.ts";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 const router = createRouter({ routeTree });
 
 declare module "@tanstack/react-router" {
@@ -11,5 +17,7 @@ declare module "@tanstack/react-router" {
 }
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
 );
