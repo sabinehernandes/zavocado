2befb0cbeb1bf1860c776d692f6656372b78f3d7
Sabine <sabinehernandes@gmail.com>
2024-11-20 18:13:18 +0100

    change prices on sql seeder

--- backend/src/main/resources/avocados.sql
+++ backend/src/main/resources/avocados.sql
@@ -1,35 +1,35 @@
 INSERT INTO avocados(avocados_id, name, price, description, image) VALUES('30f78b6d-3f01-4f96-ad0c-d87c723502f0',
                                                                           'Bacon',
-                                                                          '52',
+                                                                          '5.2',
                                                                           'A variety with thin, light green skin and a milder, less buttery flavor',
                                                                           'bacon.jpg');
 INSERT INTO avocados(avocados_id, name, price, description, image) VALUES('743bc661-ab13-4c1b-af90-5f11fe03db21',
                                                                           'Fuerte',
-                                                                          '63',
+                                                                          '6.3',
                                                                           'Pear-like shape and smooth, green skin. Creamy with a slightly nutty flavor',
                                                                           'fuerte.jpg');
 INSERT INTO avocados(avocados_id, name, price, description, image) VALUES('3ed96650-d0ab-4cad-bf03-04ac8a4a0d76',
                                                                           'Hass',
-                                                                          '48',
+                                                                          '5.60',
                                                                           'Dark green to black skin and creamy, rich-flavored flesh',
                                                                           'hass.jpg');
 INSERT INTO avocados(avocados_id, name, price, description, image) VALUES('1321ae68-e8f9-4491-9f5a-3d226d4ffaf5',
                                                                           'Lamb-Hass',
-                                                                          '57',
+                                                                          '5.7',
                                                                           'Similar to Hass but larger and firmer. Creamy texture and rich taste',
                                                                           'lamb-hass.jpg');
 INSERT INTO avocados(avocados_id, name, price, description, image) VALUES('cc175048-2007-4e22-b0aa-722fa950cc91',
                                                                           'Pinkerton',
-                                                                          '78',
+                                                                          '7.8',
                                                                           'A long, pear-shaped with a thick green skin. Rich, creamy texture',
                                                                           'pinkerton.jpg');
 INSERT INTO avocados(avocados_id, name, price, description, image) VALUES('59820460-f1cb-4579-b12f-2ab963e8503b',
                                                                           'Reed',
-                                                                          '62',
+                                                                          '6.2',
                                                                           'Round-shaped with thick, green skin. Buttery and smooth flavor, ideal for guacamole',
                                                                           'reed.jpg');
 INSERT INTO avocados(avocados_id, name, price, description, image) VALUES('ffe42758-9274-4060-9056-046b2aa4fa38',
                                                                           'Zutano',
-                                                                          '70',
+                                                                          '7.0',
                                                                           'A lighter, less creamy avocado with shiny, thin green skin and a mild flavor',
                                                                           'zutano.jpg');
