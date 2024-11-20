74aced2c555912eba48f5fbbb5591b1e7506c935
Sabine <sabinehernandes@gmail.com>
2024-11-20 18:38:24 +0100

    increase pie chart max height

--- frontend/src/components/admin/ItemsSoldChart.tsx
+++ frontend/src/components/admin/ItemsSoldChart.tsx
@@ -66,7 +66,7 @@ export function ItemsSoldChart() {
       <CardContent className="flex-1 pb-0">
         <ChartContainer
           config={chartConfig}
-          className="mx-auto aspect-square max-h-[250px]"
+          className="mx-auto aspect-square max-h-[360px]"
         >
           <PieChart>
             <ChartTooltip
