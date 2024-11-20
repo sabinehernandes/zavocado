222c897534711c73cc48dd8e695fbfe1cbdea37d
Sabine <sabinehernandes@gmail.com>
2024-11-20 10:13:57 +0100

    update ProductsPage to use useQuery hook from tanstack

--- frontend/src/components/products-page/ProductsPage.tsx
+++ frontend/src/components/products-page/ProductsPage.tsx
@@ -1,6 +1,28 @@
+import { getAvocados } from "@/services/api";
 import ProductCard from "./ProductCard";
+import { useQuery } from "@tanstack/react-query";
+
+interface Avocado {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+}
 
 export default function ProductsPage() {
+  const {
+    data: avocados,
+    error,
+    isLoading,
+  } = useQuery<Avocado[]>({
+    queryKey: ["avocados"],
+    queryFn: getAvocados,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading avocados</p>;
+
   return (
     <>
       <main className="bg-slate-100 text-center h-96 flex flex-col items-center justify-center space-y-4">
@@ -9,10 +31,15 @@ export default function ProductsPage() {
         </h1>
       </main>
       <section className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-3 lg:grid-rows-2 gap-3 m-10">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {avocados?.map((avocado) => (
+          <ProductCard
+            key={avocado.id}
+            name={avocado.name}
+            price={avocado.price}
+            description={avocado.description}
+            imageUrl={avocado.image}
+          />
+        ))}
       </section>
     </>
   );
