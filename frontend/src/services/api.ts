0b84e932eabaab51128c2b135cf91edb6bc7e9cb
Sabine <sabinehernandes@gmail.com>
2024-11-20 13:49:28 +0100

    setup form states and queryClient

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -5,7 +5,7 @@ const base_url = import.meta.env.VITE_BASE_URL;
 export const getAvocados = async () => {
   try {
     const response = await axios.get(base_url);
-    //console.log(response);
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error(`Error getting avocados: ${error}`);
@@ -63,13 +63,12 @@ export const updateAvocado = async (
   }
 };
 
-updateAvocado(
-  "87dc32b4-7e95-4c17-a298-5c57146842da",
-  "updated Hass Avocado",
-  2.0,
-  "A rich and creamy avocado.",
-  "image_url"
-);
-
+// updateAvocado(
+//   "87dc32b4-7e95-4c17-a298-5c57146842da",
+//   "updated Hass Avocado",
+//   2.0,
+//   "A rich and creamy avocado.",
+//   "image_url"
+// );
 //getAvocados();
 //deleteAvocado("7f80ba08-abf8-4688-ab2a-aa7cb9ae393c");
