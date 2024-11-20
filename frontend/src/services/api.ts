e242c676afb0aea8eb123223241e2adce053d211
Sabine <sabinehernandes@gmail.com>
2024-11-20 16:28:23 +0100

    add toast for success and error inside all methods

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,24 +1,24 @@
 import axios from "axios";
+import toast from "react-hot-toast";
 
 const base_url = import.meta.env.VITE_BASE_URL;
 
 export const getAvocados = async () => {
   try {
     const response = await axios.get(base_url);
-    console.log(response.data);
     return response.data;
   } catch (error) {
-    console.error(`Error getting avocados: ${error}`);
+    toast.error("Error fetching items. Please try again later");
   }
 };
 
 export const deleteAvocado = async (id: string) => {
   try {
     const response = await axios.delete(`${base_url}/${id}`);
-    console.log(`Avocado deleted id: ${id}`);
+    toast.success("Item successfully deleted");
     return response.data;
   } catch (error) {
-    console.error(`Error deleting avocado id: ${id}`);
+    toast.error("Error deleting item. Please try again later");
   }
 };
 
@@ -42,10 +42,10 @@ export const createAvocado = async ({
       description,
       image,
     });
-    console.log(`Avocado created successfully.`);
+    toast.success("Item successfully created  🥑");
     return response.data;
   } catch (error) {
-    console.error(`Error creating avocado: ${error}`);
+    toast.error("Error creating item. Please try again later");
   }
 };
 
@@ -63,9 +63,9 @@ export const updateAvocado = async (
       description,
       image,
     });
-    console.log(`Avocado with ID ${id} updated successfully.`);
+    toast.success("Item successfully updated 🥑");
     return response.data;
   } catch (error) {
-    console.error(`Error updating avocado with ID ${id}:`, error);
+    toast.error("Error updating item. Please try again later");
   }
 };
