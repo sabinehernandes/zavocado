6b5d2eb81c0350d03b3d2f72ae775a71c2d481b0
Sabine <sabinehernandes@gmail.com>
2024-11-20 11:04:22 +0100

    add resource handler to properly deal with pictures

--- backend/src/main/java/zavocado/backend/configuration/WebAppConfig.java
+++ backend/src/main/java/zavocado/backend/configuration/WebAppConfig.java
@@ -3,6 +3,7 @@ package zavocado.backend.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -16,5 +17,12 @@ public class WebAppConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Map /static/** to static resources
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
